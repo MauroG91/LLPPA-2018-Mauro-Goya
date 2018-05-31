7ce32c9e66bf976afd499a69757e67bbf3826f23
MauroG91 <goyamauro@gmail.com>
2018-05-31 12:02:33 -0300

    Press button for change the color

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log ('hello');
+
+var changeColor = function (li) {
+  li.style.backgroundColor = '#F00';
+}
+
+window.onload = function () {
+  var btnPlay = document.getElementById('play');
+  btnPlay.onclick = function () {
+    var liAlives = document.getElementsByClassName('alive');
+    for (var i = 0; i < liAlives.length; i++) {
+      changeColor(liAlives[i]);
+    }
+  }
+}
